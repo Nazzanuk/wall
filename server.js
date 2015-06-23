f26031b89c2e389fdb7782a28018ff65d607e209
Nathan N <nazzanuk@gmail.com>
2015-06-23 21:17:05 +0100

    :D

--- server.js
+++ server.js
@@ -122,11 +122,12 @@ var insertNote = function (fields, callback) {
 };
 
 
-var server = app.listen(4000, function () {
+var port = process.env.PORT || 8080;
 
-    //var host = server.address().address;
+var server = app.listen(process.env.PORT, function () {
+
+    var host = server.address().address;
     //var port = server.address().port;
-    var port = process.env.PORT || 4000;
 
-    //console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
